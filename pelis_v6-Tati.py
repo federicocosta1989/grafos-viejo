29f7cc97569b408c770791fa83c29f56d60fd19f
t87hartinger <thartinger@cognitiva.la>
2018-03-05 15:10:50 -0300

    Agrego una version del codigo

---
+++ pelis_v6-Tati.py
@@ -0,0 +1,343 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 09:39:19 2018
+
+@author: equipoGrafos
+"""
+
+#%%
+import json
+#import watson_developer_cloud
+from watson_developer_cloud import ConversationV1
+
+#para obtener las credenciales, dentro de un workspace ir a deploy->credentials
+
+user = '0eafd8c8-3aba-486a-8c39-0d9919459589'
+pass_ = 'Lw8yNivJxfCF'
+ver = '2018-02-16'
+wks_id = '1825639a-2ccf-45ca-af2f-dbae43426489'
+
+conversation = ConversationV1(username=user,password=pass_,version=ver)
+#%%
+from py2neo import Graph
+from py2neo import Node, Relationship
+from py2neo import cypher
+
+# graph_1 = Graph()
+# graph_2 = Graph(host="localhost")
+graph = Graph(password='grafos')
+#%%
+#Creamos los nodos:
+
+def crear_nodos_desde_csv(nombre):
+    """ nombre debe ir en singular y con las inciales en mayusculas. """
+    graph.data("LOAD CSV WITH HEADERS FROM 'file:///" + nombre.lower() + ".csv' AS line MERGE (" + nombre + ":" + nombre + "{ " + nombre.lower() + ":line." + nombre.lower() + " })")
+
+#Creo un nodo INICIO desde donde vamos a arrancar a preguntar:
+graph.data("create (i:Inicio {nombre:'inicio'})")
+
+crear_nodos_desde_csv("Pregunta")
+
+crear_nodos_desde_csv("Profesion")
+crear_nodos_desde_csv("Sexo")
+crear_nodos_desde_csv("Estado_Civil")
+crear_nodos_desde_csv("Cantidad_Hijos")
+crear_nodos_desde_csv("Edad")
+crear_nodos_desde_csv("Comida")
+crear_nodos_desde_csv("Lugar_Nacimiento")
+crear_nodos_desde_csv("Deporte")
+
+crear_nodos_desde_csv("Persona")
+#-------------------------------------------------------------
+#Creamos las aristas:
+
+def crear_aristas(categoria):
+    graph.data("match(p:Pregunta),(x:" + categoria + ") where p.pregunta='" + categoria.lower() + "' merge (p)-[:ES]->(x)")
+
+crear_aristas("Sexo")
+crear_aristas("Profesion")
+crear_aristas("Estado_Civil")
+crear_aristas("Cantidad_Hijos")
+crear_aristas("Edad")
+crear_aristas("Comida")
+crear_aristas("Lugar_Nacimiento")
+crear_aristas("Deporte")
+
+""" FALTA MEJORAR """
+#Aristas entre nodos de personas y los nodos que corresponden a la respuesta que dio cada persona a las distintas preguntas:
+graph.data("LOAD CSV WITH HEADERS FROM 'file:///nodos_personas.csv' AS line FIELDTERMINATOR ';' MERGE (p:Persona { persona:line.persona }) MERGE (s:Sexo { sexo:line.sexo }) MERGE (e:Estado_Civil { estado_civil:line.estado_civil }) MERGE (h:Cantidad_Hijos { cantidad_hijos:line.cantidad_hijos }) MERGE (pr:Profesion { profesion:line.profesion }) MERGE (ed:Edad { edad:line.edad }) MERGE (d:Deporte { deporte:line.deporte }) MERGE (c:Comida { comida:line.comida }) MERGE (l:Lugar_Nacimiento { lugar_nacimiento:line.lugar_nacimiento }) MERGE (s)-[:CORRESPONDE_A]->(p) MERGE (e)-[:CORRESPONDE_A]->(p) MERGE (h)-[:CORRESPONDE_A]->(p) MERGE (pr)-[:CORRESPONDE_A]->(p) MERGE (ed)-[:CORRESPONDE_A]->(p) MERGE (d)-[:CORRESPONDE_A]->(p) MERGE (c)-[:CORRESPONDE_A]->(p) MERGE (l)-[:CORRESPONDE_A]->(p)")
+
+#Aristas entre el nodo ficticio Inicio y los nodos con las posibles preguntas:
+graph.data("MATCH(i:Inicio),(pr:Pregunta) WHERE i.nombre='inicio' MERGE (i)-[w:PREGUNTAR]->(pr)")
+
+#%%
+def sumaDeLista(lista):
+    sum = 0
+    for i in lista:
+        sum = sum + i
+    return sum
+
+def metrica_2(lista):
+    return (sumaDeLista(lista) - max(lista))
+
+def lista_de_preguntas():
+    lista = []
+    query = "match(i:Inicio)-[:PREGUNTAR]->(x:Pregunta)-[:ES]->(a) where i.nombre='inicio' return distinct labels(a)"
+    aux = graph.data(query)
+    for item in aux:
+        lista.append(item["labels(a)"][0])
+    return lista   
+
+def lista_de_valores_de_categoria(categoria):
+    query = "match(p:Pregunta)-[:ES]->(x:" + categoria + ") where p.pregunta='"+ categoria.lower() + "' return distinct x." + categoria.lower()
+    lista_aux = list(graph.data(query))
+    lista = []
+    for item in lista_aux:
+        lista.append(item["x." + categoria.lower()])
+    return lista
+
+def cant_aristas(categoria, valor_categoria):
+    query = "match(e:" + categoria + ")-[:CORRESPONDE_A]->(p:Persona) where e." + categoria.lower() + "='" + valor_categoria + "' return count(p) as cant"
+    return graph.data(query)[0]["cant"]
+
+def peso_categoria(categoria):
+    lista = []
+    for i in lista_de_valores_de_categoria(categoria):
+        lista.append( cant_aristas(categoria, i) )
+    return metrica_2(lista)
+
+def poner_peso(categoria):  
+    query = "match (i:Inicio)-[w:PREGUNTAR]->(pr:Pregunta) where i.nombre='inicio' and pr.pregunta='" + categoria.lower() +"' set w.peso=" + str(peso_categoria(categoria))
+    graph.data(query)
+    #print(graph.data(query2))
+    
+def poner_pesos():
+    for categoria in lista_de_preguntas():
+        poner_peso(categoria)
+        
+def lista_pesos():    
+    lista_pes = []
+    for categoria in lista_de_preguntas():
+        query2 = "match (i:Inicio)-[w:PREGUNTAR]->(pr:Pregunta) where i.nombre='inicio' and pr.pregunta='" + categoria.lower() +"' return w.peso"
+        lista_pes.append( graph.data(query2)[0]["w.peso"])
+    return lista_pes 
+
+def elegir_mejor_pregunta():
+    poner_pesos()
+    return lista_de_preguntas()[lista_pesos().index(max(lista_pesos()))]
+
+def borrar_nodos(pregunta, respuesta_usuario):
+    #pregunta =elegir_mejor_pregunta()
+    
+    """ QUERYS INFORMATIVAS """
+    query = "MATCH (i:Inicio { nombre: 'inicio' })-[w]->(p:Pregunta { pregunta: '" + pregunta.lower() + "' })-[r]->(v:"+ pregunta +")-[a]->(persona) where v." + pregunta.lower() + " <> '" + respuesta_usuario + "' RETURN count(persona)"
+    print("Se borraron " + str(graph.data(query)[0]["count(persona)"]) + " nodos Persona")
+    query2 = "MATCH (i:Inicio { nombre: 'inicio' })-[w]->(p:Pregunta { pregunta: '" + pregunta.lower() + "' })-[r]->(v:"+ pregunta +") where v." + pregunta.lower() + " <> '" + respuesta_usuario + "' RETURN count(v)"
+    print("Se borraron " + str(graph.data(query2)[0]["count(v)"]) + " nodos " + pregunta)
+    #query10 = "MATCH (i:Inicio { nombre: 'inicio' })-[w]->(p:Pregunta { pregunta: '" + pregunta.lower() + "' })-[r]->(v:"+ pregunta +") where v." + pregunta.lower() + " <> '" + respuesta_usuario + "' RETURN v"
+    #j=graph.data(query10)
+    query3 = "MATCH (v:"+ pregunta +") where v." + pregunta.lower() + " = '" + respuesta_usuario + "' RETURN count(v)"
+    print("Se borraron " + str(graph.data(query3)[0]["count(v)"]) + " nodos " + pregunta)
+    query4 = "MATCH (p:Pregunta) where p.pregunta = '" + pregunta.lower() + "' RETURN count(p)"
+    print("Se borraron " + str(graph.data(query4)[0]["count(p)"]) + " nodos Pregunta")
+    
+    """ QUERYS DE BORRADO """
+    query5 = "MATCH (i:Inicio { nombre: 'inicio' })-[w]->(p:Pregunta { pregunta: '" + pregunta.lower() + "' })-[r]->(v:"+ pregunta +")-[a]->(persona) where v." + pregunta.lower() + " <> '" + respuesta_usuario + "' DETACH DELETE persona"
+    graph.data(query5) #se eliminan los nodos personas que no sean incidentes a respuesta_usuario, mas los nodos que ya no serviran
+    
+    query6 = "MATCH (v:" + pregunta + ") DETACH DELETE v"
+    graph.data(query6)
+    
+    query7 = "MATCH (p:Pregunta { pregunta: '" + pregunta.lower() + "' }) DETACH DELETE p"
+    graph.data(query7)
+
+def contar_nodos(label):
+    """ label_del_nodo puede ser cualquiera de los siguientes strings: "Inicio", "Pregunta", "Profesion", ... , "Persona" """ 
+    query = "MATCH (" + label.lower() + ":" + label+ ") RETURN count(" + label.lower() + ")"
+    return graph.data(query)[0]["count(" + label.lower() + ")"]
+
+def lista_labels():
+    query = "match (i) return distinct labels(i)"
+    lista = []
+    aux = graph.data(query)
+    for item in aux:
+        lista.append(item["labels(i)"][0])
+    return lista
+        
+def status():
+    dict_ = {}
+    for item in lista_labels():
+        dict_[item]=contar_nodos(item)
+    return dict_
+
+
+#%%%
+#Conversando con Watson
+        
+comida = []
+sexo = []
+contexto={"pregunta": ""}
+
+contexto={"deporte": ""}
+contexto={"edad": ""}
+contexto={"sexo": ""}
+contexto={"estado_civil": ""}
+contexto={"lugar_nacimiento": ""}
+contexto={"comida": ""}
+contexto={"cantidad_hijos": ""}
+contexto={"profesion": ""}
+
+response = {}
+output_text = "Presione Enter para iniciar la conversación."
+preg_no_realizadas = lista_de_preguntas()
+entidades = []
+preg_realizadas = []
+salir =False
+
+while True:
+    
+    if not("output" in response) or response["output"]["nodes_visited"][-1] != "node_17_1519053529305":
+    
+        input_text = input(output_text)
+        
+        if input_text not in ("Exit", "exit"):
+            
+            response = conversation.message(
+                workspace_id = wks_id,
+                input = {'text': input_text},
+                context = contexto)
+            
+#            print(json.dumps(response, indent=2))
+            
+            pregunta = elegir_mejor_pregunta()
+            
+            if pregunta in preg_realizadas:
+                print(pregunta + " esta dentro de las preguntas realizadas asi que la borro del context")
+                response["context"]["pregunta"] = ""
+#                print(json.dumps(response, indent=2))
+                
+            respuesta = response["context"][pregunta.lower()]
+            print("la respuesta a " + pregunta + " es: ")
+            print(respuesta)
+
+            
+            if response["output"]["nodes_visited"][-1] == "node_7_1520016504617":
+                print("entro en el if")
+                print("la mejor pregunta es: ")
+                print(elegir_mejor_pregunta())
+                response["context"]["pregunta"] = elegir_mejor_pregunta()
+                preg_realizadas.append(elegir_mejor_pregunta())
+                
+            if (pregunta in response["context"]["pregunta"] and response["context"][pregunta.lower()] != ""):
+                print("entro en el segundo if")
+                respuesta = response["context"][pregunta.lower()]
+                print("la respuesta a " + pregunta + " es:")
+                print(respuesta)
+#                print(json.dumps(response,indent=2))
+                borrar_nodos(pregunta,respuesta)
+                response["context"][pregunta.lower()]=""
+#                print(json.dumps(response,indent=2))
+                print(status())
+#                salir = True
+                
+            contexto = response["context"]
+            output_text = response["output"]["text"]
+#            if salir:
+#                break
+        else:
+            break
+#%%              
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+            """
+            if ("comida" in response["context"] and "Comida" in preg_no_realizadas):
+                True
+            if ("sexo" in response["context"]):
+                sexo.append(response["context"]["sexo"]) 
+                """
+                
+            """
+        else:
+        #query="MATCH(actor:Person)-[:ACTED_IN]->(movie:Movie)<-[:DIRECTED]-(director:Person) WHERE actor.name=" + "'" + actores[0] + "'" + "AND director.name=" + "'" + directores[0] + "'" +" AND movie.genre=" + "'" + genero[0] + "'" + " RETURN DISTINCT movie.title LIMIT 1;"
+        queryMATCH = "MATCH(director:Person)-[:DIRECTED]->(movie:Movie)<-[:ACTED_IN]-(actor:Person)"
+        queryWHERE = "WHERE "
+        queryActores= "actor.name=" + "'" + actores[0] +"'"
+        queryAND = " AND "
+        queryDirectores = "director.name=" + "'" + directores[0] +"'"
+        queryGeneros = "movie.genre=" + "'" + genero[0] +"'"
+        queryRETURN = " RETURN DISTINCT movie.title AS Title LIMIT 1;"
+        
+        query= queryMATCH + queryWHERE + queryActores + queryAND + queryDirectores + queryAND + queryGeneros + queryRETURN
+
+        hicho=graph.data(query)
+        print(hicho)
+        break
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
